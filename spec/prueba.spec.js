fc7e13bd0866718dac842cf18274a7916776298f
brianpadilla3072 <padillabrian830@gmail.com>
2024-04-03 01:58:05 -0300

    test clase activity - creacion de instancia

--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -2,12 +2,12 @@ const {activity, repository_activity} = require("../scripts/index")
 describe("Clase activity", function() {
   // Prueba para crear una instancia de activity
   it("debería crear una instancia de activity con los parámetros adecuados", function() {
-    const exampleActivity = new activity("Título de ejemplo", "Descripción de ejemplo", "https://example.com", 1);
+    const exampleActivity = new activity("Título de ejemplo", "Descripción de ejemplo", "https://ejemplo.com", 1);
 
     // Verificar si la instancia se crea correctamente
     expect(exampleActivity.title).toEqual("Título de ejemplo");
     expect(exampleActivity.description).toEqual("Descripción de ejemplo");
-    expect(exampleActivity.url).toEqual("https://example.com");
+    expect(exampleActivity.url).toEqual("https://ejemplo.com");
     expect(exampleActivity.id).toEqual(1);
   });
 });
